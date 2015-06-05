5e2e55b2fc1409d63e9e96904814b9c0dd436874
nicholascloud <me@nicholascloud.com>
2015-06-05 12:42:11 -0500

    validators/lt tests

--- src/validators/lt.js
+++ src/validators/lt.js
@@ -3,10 +3,16 @@ var compare = require('../compare');
 
 module.exports = function lt (number, inclusive, message) {
   number = Number(number);
+  if (isNaN(number)) {
+    throw new TypeError('number must be numeric');
+  }
   inclusive = inclusive || false;
-  message = message || ('field must be less than ' + number);
+  message = message || 'value must be less than ' + number;
   return function (adapter) {
     var value = Number(adapter.value());
+    if (isNaN(value)) {
+      return 'value must be numeric';
+    }
     if (!compare(inclusive).lt(value, number)) {
       return message;
     }
