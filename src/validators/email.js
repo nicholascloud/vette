a303bda7697d2dd3c4c4e743da0f1a2425238c92
nicholascloud <me@nicholascloud.com>
2015-06-04 19:47:07 -0500

    validators/email tests

--- src/validators/email.js
+++ src/validators/email.js
@@ -4,5 +4,6 @@ var match = require('./match');
 var EMAIL_REGEX = /^[^(@|\s)]+@[^(@|\s)]+\.[a-z0-9]+$/i;
 
 module.exports = function email (message) {
+  message = message || 'email address is invalid';
   return match(EMAIL_REGEX, message);
 };
