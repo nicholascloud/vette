27ef410fe5bf533566330bee0b6475d50482e54c
nicholascloud <me@nicholascloud.com>
2015-06-05 11:53:18 -0500

    added placeholder for index tests

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+var expect = require('chai').expect;
+
+var Vette = require('../src/index');
+
+describe('index', function () {
+
+  it('TODO add index tests', function (done) {
+    done();
+  });
+});
