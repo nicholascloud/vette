0f98f025530eb24ff838e4639403e93a0797afb9
nicholascloud <me@nicholascloud.com>
2015-06-04 20:07:50 -0500

    validators/gteq tests

--- src/validators/gteq.js
+++ src/validators/gteq.js
@@ -3,6 +3,9 @@ var gt = require('./gt');
 
 module.exports = function gteq (number, message) {
   number = Number(number);
-  message = message || ('field must be greater than or equal to ' + number);
+  if (isNaN(number)) {
+    throw new TypeError('number must be numeric');
+  }
+  message = message || 'value must be greater than or equal to ' + number;
   return gt(number, true, message);
 };
--- test/valiators/gteq.test.js
+++ test/valiators/gteq.test.js
@@ -6,8 +6,61 @@ var gteqValidator = require('../../src/validators').gteq;
 
 describe('gteq', function () {
 
-  it('', function (done) {
+  it('throws an error if number is not numeric', function (done) {
+    var fn = function () {
+      gteqValidator();
+    };
+    expect(fn).to.throw(TypeError, /number/);
+    done();
+  });
 
+  it('returns a message if value is not numeric', function (done) {
+    var expectedMessage = 'value must be numeric';
+    var obj = {
+      foo: 'not numeric'
+    };
+    var rootAdapter = hashAdapter(obj);
+    var fooAdapter = rootAdapter.find('foo');
+    var rule = gteqValidator(1);
+    var actualMessage = rule(fooAdapter);
+    expect(actualMessage).to.equal(expectedMessage);
+    done();
+  });
+
+  it('returns a default message if value is less that number', function (done) {
+    var expectedMessage = 'value must be greater than or equal to 1';
+    var obj = {
+      foo: 0
+    };
+    var rootAdapter = hashAdapter(obj);
+    var fooAdapter = rootAdapter.find('foo');
+    var rule = gteqValidator(1);
+    var actualMessage = rule(fooAdapter);
+    expect(actualMessage).to.equal(expectedMessage);
+    done();
+  });
+
+  it('returns undefined if value is equal to the number', function (done) {
+    var obj = {
+      foo: 1
+    };
+    var rootAdapter = hashAdapter(obj);
+    var fooAdapter = rootAdapter.find('foo');
+    var rule = gteqValidator(1);
+    var actualMessage = rule(fooAdapter);
+    expect(actualMessage).to.be.undefined;
+    done();
+  });
+
+  it('returns undefined if value is greater than the number', function (done) {
+    var obj = {
+      foo: 2
+    };
+    var rootAdapter = hashAdapter(obj);
+    var fooAdapter = rootAdapter.find('foo');
+    var rule = gteqValidator(1);
+    var actualMessage = rule(fooAdapter);
+    expect(actualMessage).to.be.undefined;
     done();
   });
 });
