55020bd531e9c2627160874933240b9e4983cc2c
nicholascloud <me@nicholascloud.com>
2015-06-04 20:01:46 -0500

    validators/gt tests

--- src/validators/gt.js
+++ src/validators/gt.js
@@ -3,10 +3,16 @@ var compare = require('../compare');
 
 module.exports = function gt (number, inclusive, message) {
   number = Number(number);
+  if (isNaN(number)) {
+    throw new TypeError('number must be numeric');
+  }
   inclusive = inclusive || false;
-  message = message || ('field must be greater than ' + number);
+  message = message || 'value must be greater than ' + number;
   return function (adapter) {
     var value = Number(adapter.value());
+    if (isNaN(value)) {
+      return 'value must be numeric';
+    }
     if (!compare(inclusive).gt(value, number)) {
       return message;
     }
