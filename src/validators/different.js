f01b688b4222b7cb815396bf9d259d942a8431b3
nicholascloud <me@nicholascloud.com>
2015-06-04 19:42:38 -0500

    validators/different tests

--- src/validators/different.js
+++ src/validators/different.js
@@ -1,7 +1,11 @@
 'use strict';
+var MissingArgumentError = require('../errors').MissingArgumentError;
+
 module.exports = function different (selector, message) {
-  selector = selector || '';
-  message = message || 'field must be different';
+  if (!selector) {
+    throw new MissingArgumentError('selector');
+  }
+  message = message || 'values must be different';
   return function (adapter, rootAdapter) {
     var otherAdapter = rootAdapter.find(selector);
     if (adapter.value() === otherAdapter.value()) {
