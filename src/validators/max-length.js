4cc2e7bd684ae1c668021e29362c4911120b7b5f
nicholascloud <me@nicholascloud.com>
2015-06-12 10:47:08 -0500

    validators/max-length returns errors

--- src/validators/max-length.js
+++ src/validators/max-length.js
@@ -1,5 +1,6 @@
 'use strict';
 var isNumber = require('../is-number');
+var ValidatorError = require('../errors').ValidatorError;
 
 module.exports = function maxLength (length, message) {
   length = Number(length);
@@ -9,7 +10,7 @@ module.exports = function maxLength (length, message) {
   message = message || 'value has a maximum length of ' + length;
   return function (adapter) {
     if (adapter.value().length > length) {
-      return message;
+      return new ValidatorError(message);
     }
   };
 };
