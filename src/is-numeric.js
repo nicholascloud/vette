27fae4418d3086fbf12eaa8dfcaf118a44313516
nicholascloud <me@nicholascloud.com>
2015-06-16 10:49:18 -0500

    changed is-numeric to use type classes

--- src/is-numeric.js
+++ src/is-numeric.js
@@ -1,9 +1,15 @@
 'use strict';
+var NULL_CLASS = '[object Null]';
+var UNDEFINED_CLASS = '[object Undefined]';
 var NUM_REGEX = /^[-]?[0-9]+$/;
 
+function is(target, typeClass) {
+  return Object.prototype.toString.call(target) === typeClass;
+}
+
 module.exports = function isNumeric (target) {
-  return target !== null &&
-      typeof target !== 'undefined' &&
+  return !is(target, NULL_CLASS) &&
+      !is(target, UNDEFINED_CLASS) &&
       target.toString()
         .trim()
         .search(NUM_REGEX) > -1;
